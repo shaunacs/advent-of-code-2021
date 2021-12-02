94e19b93a031974b55d563cb719643b079b32c0a
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-02 18:28:07 -0500

    Completed day 1 part 2

--- day-1/day_1.py
+++ day-1/day_1.py
@@ -44,3 +44,27 @@ def make_windows_of_three(input_file):
             prev_num = sum(window)
             
     print(increased)
+
+
+def check_every_three(input_file):
+    measurements = open(input_file)
+
+    measurements_lst = []
+    for line in measurements:
+        measurements_lst.append(int(line.strip()))
+
+    increased = 0
+    prev_num = None
+
+    for num in range(len(measurements_lst) - 2):
+        if prev_num is not None:
+            window_range = [measurements_lst[num], measurements_lst[num + 1], measurements_lst[num + 2]]
+            sum_of_window = sum(window_range)
+            if sum_of_window > prev_num:
+                increased += 1
+            prev_num = sum_of_window
+        else:
+            window_range = [measurements_lst[num], measurements_lst[num + 1], measurements_lst[num + 2]]
+            prev_num = sum(window_range)
+        
+    return increased
