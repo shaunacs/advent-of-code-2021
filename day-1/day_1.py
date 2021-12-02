18298936f0994fd90b33e46699b6a0ecdfd1441d
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-02 18:01:29 -0500

    Makes window of three but not the task

--- day-1/day_1.py
+++ day-1/day_1.py
@@ -17,3 +17,30 @@ def find_num_increases(input_file):
     return increased
 
 
+def make_windows_of_three(input_file):
+    measurements = open(input_file)
+
+    windows_of_three = []
+
+    for line in measurements:
+        if windows_of_three == []:
+            windows_of_three.append([int(line)])
+        elif len(windows_of_three[-1]) < 3:
+            windows_of_three[-1].append(int(line))
+        else:
+            windows_of_three.append([int(line)])
+    
+    increased = 0
+
+    prev_num = None
+
+    for window in windows_of_three:
+        if prev_num is not None and len(window) == 3:
+            sum_of_window = sum(window)
+            if sum_of_window > prev_num:
+                increased += 1
+            prev_num = sum_of_window
+        else:
+            prev_num = sum(window)
+            
+    print(increased)
