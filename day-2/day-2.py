c4425be53873885a726ac564f3f4471979144506
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-15 13:05:34 -0500

    Add function to make list of directions

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -1,4 +1,11 @@
 def get_puzzle_input(input_file):
     """Takes in puzzle input as text file and returns list of directions"""
 
-    
+    day_2_input = open(input_file)
+
+    directions = []
+
+    for line in day_2_input:
+        directions.append(line.rstrip())
+
+    return directions
