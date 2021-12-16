e7cd464b26bad13def44451a8dcc5f7afd7234e2
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-16 18:33:35 -0500

    Complete day 2 part 1

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -17,10 +17,29 @@ def split_command(command):
     return command.split()
 
 
+def execute_commands(command, location):
+    """Takes in a list command (Ex. ['up', 3]) and location, and updates location accordingly"""
+
+    movement_num = int(command[1])
+
+    if command[0] == 'forward':
+        location['horizontal'] += movement_num
+    elif command[0] == 'down':
+        location['depth'] += movement_num
+    elif command[0] == 'up':
+        location['depth'] -= movement_num
+    
+    return location
 
 
+
+
+starting_location = {'horizontal': 0,
+                    'depth': 0}
 
 directions_lst = get_puzzle_input('day-2-input.txt')
 
 for direction in directions_lst:
-    print(split_command(direction))
+    execute_commands(split_command(direction), starting_location)
+
+final_answer = starting_location['horizontal'] * starting_location['depth']
