81b68b923b59741f688d5066376b1d32e968d6cb
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-16 19:48:45 -0500

    Debugging aim calculation

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -31,15 +31,49 @@ def execute_commands(command, location):
     
     return location
 
+def solve_day2_1():
+    starting_location = {'horizontal': 0,
+                        'depth': 0}
+
+    directions_lst = get_puzzle_input('day-2-input.txt')
+
+    for direction in directions_lst:
+        execute_commands(split_command(direction), starting_location)
+
+    final_answer = starting_location['horizontal'] * starting_location['depth']
+
+    return final_answer
+
+
+def execute_commands2(command, location, aim):
+
+    movement_num = int(command[1])
+
+    if command[0] == 'forward':
+        location['horizontal'] += movement_num
+        location['depth'] += (aim * movement_num)
+    elif command[0] == 'down':
+        # location['depth'] += movement_num
+        aim += movement_num
+    elif command[0] == 'up':
+        # location['depth'] -= movement_num
+        print(aim - movement_num)
+    
+    return aim
 
 
 
 starting_location = {'horizontal': 0,
                     'depth': 0}
+aim = 0
 
-directions_lst = get_puzzle_input('day-2-input.txt')
+# directions_lst = get_puzzle_input('day-2-input.txt')
+directions_lst = get_puzzle_input('test-input.txt')
 
-for direction in directions_lst:
-    execute_commands(split_command(direction), starting_location)
+for direction in directions_lst[:4]:
+    print(f'aim before = {aim}')
+    aim += execute_commands2(split_command(direction), starting_location, aim)
+    print(starting_location)
+    print(f'aim={aim}')
 
 final_answer = starting_location['horizontal'] * starting_location['depth']
