db760f443b7ccc9c54dd95ff0316016c8f728cb9
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-17 05:00:14 -0500

    Create function to count bits

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -9,4 +9,22 @@
 # return bit count
 
 def count_bits(input_file):
+    day_3_input = open(input_file)
+
+    bit_count = {}
+
+    for bin_num in day_3_input:
+        bin_num = bin_num.rstrip()
+        
+        for idx, char in enumerate(bin_num):
+            if idx in bit_count:
+                if int(char) in bit_count[idx]:
+                    bit_count[idx][int(char)] += 1
+                else:
+                    bit_count[idx][int(char)] = 1
+            else:
+                bit_count[idx] = {int(char): 1}
     
+    return bit_count
+
+print(count_bits('test-input.txt'))
